90cb237c886924906bc24e8504cf1308a4132285
Gustavo <gggamer1511@gmail.com>
2021-04-15 23:44:28 -0300

    2nd commit

--- app/src/main/java/com/jogos/adivinhador/MainActivity.kt
+++ app/src/main/java/com/jogos/adivinhador/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jogos.adivinhador
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startButton(view: View)
+    {
+        startActivity(Intent(this, Adivinhador::class.java))
     }
 }
